bd458d691bb3e2fbca3b4293875bc2c62ed289c8
chalasimon <chally.r@hotmail.com>
2025-07-03 21:26:52 +0300

    feat : feature extraction encoding and handling missiong values

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,6 +1,7 @@
 # import pandas as pd
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 class FeatureEngineering:
     def __init__(self, df: pd.DataFrame):
@@ -31,6 +32,37 @@ class FeatureEngineering:
         self.df['TransactionMonth'] = self.df['TransactionStartTime'].dt.month
         self.df['TransactionYear'] = self.df['TransactionStartTime'].dt.year
         return self.df
+    def encode_categorical_variables(self,df):
+        # Identify categorical columns
+        self.df = df
+        categorical_cols = ['ProductCategory', 'ProviderId', 'ChannelId']
+        
+        # One-Hot Encoding for categorical variables
+        one_hot_encoder = OneHotEncoder(sparse_output=False, drop='first')
+        one_hot_encoded = one_hot_encoder.fit_transform(self.df[categorical_cols])
+        one_hot_df = pd.DataFrame(one_hot_encoded, columns=one_hot_encoder.get_feature_names_out(categorical_cols))
+        
+        # Label Encoding for categorical variables
+        label_encoder = LabelEncoder()
+        for col in categorical_cols:
+            self.df[col] = label_encoder.fit_transform(self.df[col])
+        
+        # Concatenate the original DataFrame with the one-hot encoded DataFrame
+        self.df = pd.concat([self.df, one_hot_df], axis=1)
+        
+        return self.df
+    def handle_missing_values(self, df):
+        # Handle missing values by filling with the mean for numerical columns
+        numerical_cols = df.select_dtypes(include=[np.number]).columns.tolist()
+        for col in numerical_cols:
+            df[col].fillna(df[col].mean(), inplace=True)
+        
+        # Handle missing values by filling with the mode for categorical columns
+        categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
+        for col in categorical_cols:
+            df[col].fillna(df[col].mode()[0], inplace=True)
+        
+        return df
     
     
         
